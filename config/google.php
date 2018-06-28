ddd724972dd8708e5ba90845d596be608bbc783a
vagrant <vagrant@homestead>
2018-06-28 03:26:13 +0000

    config:google,app(.lock)

---
+++ config/google.php
@@ -0,0 +1,13 @@
+<?php
+return[
+  "type" => env('GOOGLE_TYPE'),
+  "project_id" => env('GOOGLE_PROJECT_ID'),
+  "private_key_id" => env('GOOGLE_PRIVATE_KEY_ID'),
+  "private_key" => env('GOOGLE_PRIVATE_KEY'),
+  "client_email" => env('GOOGLE_CLIENT_EMAIL'),
+  "client_id" => env('GOOGLE_CLIENT_ID'),
+  "auth_uri" => env('GOOGLE_AUTH_URI'),
+  "token_uri" => env('GOOGLE_TOKEN_URI'),
+  "auth_provider_x509_cert_url" => env('GOOGLE_AUTH_PROVIDER_X509_CERT_URL'),
+  "client_x509_cert_url" => env('GOOGLE_CLIENT_X509_CERT_URL'),
+];
